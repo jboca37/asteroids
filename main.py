f74a405ec43cf7171db9f899380c6156fa4a1e87
Jesus Bocanegra <jojadev@proton.me>
2025-09-03 19:51:20 -0500

    added asteroid movement

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+import pygame
+import sys
+
 from pygame.display import update
 from constants import *
 from player import Player
-import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
     player = Player(x, y)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -25,6 +36,11 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
+                print("Game Over!")
+                sys.exit()
 
         screen.fill("black")
 
